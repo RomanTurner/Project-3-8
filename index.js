3a4ae5885e43560a0c72beabb1d1b5d0a143049d
Evan <evanahouse@gmail.com>
2021-03-09 21:43:05 -0800

    Added timer, start game, and play again functionaility

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ let trumpUrl = "https://api.whatdoestrumpthink.com/api/v1/quotes/random/";
 let urlArray = [ronUrl, kanyeUrl, trumpUrl];
 let currentQuote = 0;
 let scoreCounter = 0;
-//Hey YO!
-getQuote();
-
+let counter;
+document.addEventListener('DOMContentLoaded', () => {
+  
+  let quote = document.querySelector(".quote")
+  let start = document.createElement("button")
+  start.textContent = "Click to Start!"
+  start.style.fontSize = "30px"
+  quote.appendChild(start)
+  start.setAttribute("class", "button")
+  start.addEventListener('click', () => {
+  getQuote();
+})
+})
 
 
 function getQuote(){
-  //GENERATE A RANDOM NUMBER FROM 0 TO 2
-
-
+//GENERATE A RANDOM NUMBER FROM 0 TO 2
 //(RON=0), (KANYE=1), (TRUMP=2)
+  clearInterval(counter)
+  contestantArr.forEach((contestant) => {
+  contestant.classList.remove("wrong");
+  contestant.classList.remove("correct");
+  });
   let i = Math.floor(Math.random() * 3);
   //FETCHES QUOTE BASED OFF URL ARRAY INDEX
   fetch(urlArray[i])
@@ -25,7 +38,7 @@ function getQuote(){
 }
 
 function renderQuote(data, i) {
-  console.log(i);
+  setTimer();
   let newQuote = document.querySelector(".quote");
   switch (i) {
     case 0:
@@ -40,6 +53,41 @@ function renderQuote(data, i) {
     default:
       newQuote.textContent = "Press Start to Play";
   }
+}
+
+function setTimer(){
+  let timerText = document.querySelector('.seconds')
+  let count = 21;
+  counter = setInterval(timer, 1000)
+  timer()
+  function timer(){
+    count = count - 1;
+    timerText.textContent = count 
+    if (count <= 5){
+      timerText.style.color = "red";
+      timerText.style.fontSize = "55px"; 
+    }
+    if (count <= 0){
+      displayLoss();
+      return;
+    }
+  }
+}
+
+function displayLoss(){
+  clearInterval(counter);
+  let youLose = document.querySelector(".quote")
+  let mainBox = document.querySelector(".main-box")
+  youLose.textContent = ""
+  let button = document.createElement("button")
+  button.textContent = "Play Again?"
+  button.style.fontSize = "30px"
+  youLose.appendChild(button)
+  button.setAttribute("class", "button")
+  button.addEventListener('click', () => {
+    getQuote();
+  })
+  
 }
 
 //grab div,
@@ -59,6 +107,7 @@ function incrementScore() {
 }
 
 function answer(e) {
+  clearInterval(counter)
   let y = e.path[1];
   let x = parseInt(e.path[1].dataset.num);
   if (x === currentQuote) {
@@ -69,15 +118,16 @@ function answer(e) {
 }
 
 function correct(y) {
+  clearInterval(counter)
   y.classList.add("correct");
   incrementScore();
   const nextQ = setInterval(() => nextQuestion(nextQ), 2000);
 }
 
 function wrong(y) {
+  clearInterval(counter)
   y.classList.add("wrong");
-  alert("You Lost, boo-hoo");
-  const nextQ = setInterval(() => nextQuestion(nextQ), 2000);
+  displayLoss();
   scoreCounter = -1;
   incrementScore();
 }
