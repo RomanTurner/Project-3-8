dbd9931cd1cca339e9a1fd13d110c1784f94df55
Roman Turner <rn.turner@live.com>
2021-03-11 09:07:54 -0800

    detailed message

--- index.js
+++ index.js
@@ -5,27 +5,25 @@ let urlArray = [ronUrl, kanyeUrl, trumpUrl];
 let currentQuote = 0;
 let scoreCounter = 0;
 let counter;
-document.addEventListener('DOMContentLoaded', () => {
-  
-  let quote = document.querySelector(".quote")
-  let start = document.createElement("button")
-  start.textContent = "Click to Start!"
-  start.style.fontSize = "30px"
-  quote.appendChild(start)
-  start.setAttribute("class", "button")
-  start.addEventListener('click', () => {
-  getQuote();
-})
-})
+document.addEventListener("DOMContentLoaded", () => {
+  let quote = document.querySelector(".quote");
+  let start = document.createElement("button");
+  start.textContent = "Click to Start!";
+  start.style.fontSize = "30px";
+  quote.appendChild(start);
+  start.setAttribute("class", "button");
+  start.addEventListener("click", () => {
+    getQuote();
+  });
+});
 
-
-function getQuote(){
-//GENERATE A RANDOM NUMBER FROM 0 TO 2
-//(RON=0), (KANYE=1), (TRUMP=2)
-  clearInterval(counter)
+function getQuote() {
+  //GENERATE A RANDOM NUMBER FROM 0 TO 2
+  //(RON=0), (KANYE=1), (TRUMP=2)
+  clearInterval(counter);
   contestantArr.forEach((contestant) => {
-  contestant.classList.remove("wrong");
-  contestant.classList.remove("correct");
+    contestant.classList.remove("wrong");
+    contestant.classList.remove("correct");
   });
   let i = Math.floor(Math.random() * 3);
   //FETCHES QUOTE BASED OFF URL ARRAY INDEX
@@ -55,39 +53,38 @@ function renderQuote(data, i) {
   }
 }
 
-function setTimer(){
-  let timerText = document.querySelector('.seconds')
-  let count = 21;
-  counter = setInterval(timer, 1000)
-  timer()
-  function timer(){
+function setTimer() {
+  let timerText = document.querySelector(".seconds");
+  let count = 11;
+  counter = setInterval(timer, 1000);
+  timer();
+  function timer() {
     count = count - 1;
-    timerText.textContent = count 
-    if (count <= 5){
+    timerText.textContent = count;
+    if (count <= 5) {
       timerText.style.color = "red";
-      timerText.style.fontSize = "55px"; 
+      timerText.style.fontSize = "55px";
     }
-    if (count <= 0){
+    if (count <= 0) {
       displayLoss();
       return;
     }
   }
 }
 
-function displayLoss(){
+function displayLoss() {
   clearInterval(counter);
-  let youLose = document.querySelector(".quote")
-  let mainBox = document.querySelector(".main-box")
-  youLose.textContent = ""
-  let button = document.createElement("button")
-  button.textContent = "Play Again?"
-  button.style.fontSize = "30px"
-  youLose.appendChild(button)
-  button.setAttribute("class", "button")
-  button.addEventListener('click', () => {
+  let youLose = document.querySelector(".quote");
+  let mainBox = document.querySelector(".main-box");
+  youLose.textContent = "";
+  let button = document.createElement("button");
+  button.textContent = "Play Again?";
+  button.style.fontSize = "30px";
+  youLose.appendChild(button);
+  button.setAttribute("class", "button");
+  button.addEventListener("click", () => {
     getQuote();
-  })
-  
+  });
 }
 
 //grab div,
@@ -107,7 +104,7 @@ function incrementScore() {
 }
 
 function answer(e) {
-  clearInterval(counter)
+  clearInterval(counter);
   let y = e.path[1];
   let x = parseInt(e.path[1].dataset.num);
   if (x === currentQuote) {
@@ -118,14 +115,14 @@ function answer(e) {
 }
 
 function correct(y) {
-  clearInterval(counter)
+  clearInterval(counter);
   y.classList.add("correct");
   incrementScore();
   const nextQ = setInterval(() => nextQuestion(nextQ), 2000);
 }
 
 function wrong(y) {
-  clearInterval(counter)
+  clearInterval(counter);
   y.classList.add("wrong");
   displayLoss();
   scoreCounter = -1;
